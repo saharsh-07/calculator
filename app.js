8286a66ae26ec18a778c916ceb6c301b98d691c6
saharsh-07 <109348366+saharsh-07@users.noreply.github.com>
2023-09-08 17:27:12 +0530

    final layout + dark mode fix

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const dark = document.querySelector("#dark");
+//Dark Mode 
+const dark = document.getElementById("dark");
 dark.addEventListener("click", () => {
-  const h1 = document.querySelector("h1");
-  h1.classList.toggle("dark");
   const body = document.body;
   body.classList.toggle("dark");
-  const output = document.querySelector(".output");
+  const h1 = document.querySelector("h1");
   const container = document.querySelector(".container");
-  container.classList.toggle("dark");
+  const output = document.querySelector(".output");
   const buttons = document.querySelectorAll("button");
   buttons.forEach((btn) => btn.classList.toggle("dark"))
   output.classList.toggle("dark");
+  container.classList.toggle("dark");
+  h1.classList.toggle("dark");
+  {
+  if (dark.textContent === "Dark Mode"){
+    dark.textContent = "Light Mode"
+  }
+  else{
+    dark.textContent = "Dark Mode";
+  }}
 });
+
+
+//global declarations
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const delBtn = document.querySelector("[data-del]");
+const acBtn = document.querySelector("[data-ac]");
+const prevOutput = document.querySelector("[data-prev-output]");
+const curOutput = document.querySelector("[data-cur-output]");
+const equalBtn = document.querySelector("[data-equal]");
